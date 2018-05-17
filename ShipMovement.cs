53ca6cce4473c6b37967b8ebc2689d7fe77bd768
KieliAdkins <39067815+KieliAdkins@users.noreply.github.com>
2018-05-16 23:37:53 -0700

    Add files via upload
    First (Milestone 1) upload for GPE104 assignment Sprite Mover

---
+++ ShipMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipMovement : MonoBehaviour {
+
+    // Declare the variable
+	public SpriteRenderer theRenderer; 
+
+	// Use this for initialization
+	void Start () {
+
+        // Null error avoidance
+		if (theRenderer != null) {
+			
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
